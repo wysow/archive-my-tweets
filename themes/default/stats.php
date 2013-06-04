9c9116a93a1832858e0c999678cd825bf90e29db
Gaultier BONIFACE <gboniface@wysow.fr>
2013-06-04 15:33:50 +0200

    optim on php files with php-cs-fixer

--- themes/default/stats.php
+++ themes/default/stats.php
@@ -1,15 +1,15 @@
 
-<!-- stats -->	
+<!-- stats -->
 <div class="span12">
-	<div id="stats" class="rounded">
-		
-		<div class="page-header"><h1>Stats</h1></div>
+    <div id="stats" class="rounded">
 
-		<div class="hero-unit">
-			<h1><?php echo $totalTweets; ?></h1>
-			<p class="lead">total tweets</p>
-		</div>
+        <div class="page-header"><h1>Stats</h1></div>
 
-	</div><!-- /stats -->
+        <div class="hero-unit">
+            <h1><?php echo $totalTweets; ?></h1>
+            <p class="lead">total tweets</p>
+        </div>
+
+    </div><!-- /stats -->
 </div><!-- /span12 -->
 <!-- /stats -->
--- themes/install/install02.php
+++ themes/install/install02.php
@@ -1,13 +1,13 @@
 <p class="lead">
-	Your Twitter archive has been installed!
+    Your Twitter archive has been installed!
 </p>
 
 <p>Next steps:</p>
 
 <ul>
-	<li>If you have more than 3200 tweets you'll need to <a href="https://github.com/amwhalen/archive-my-tweets#importing-your-official-twitter-archive" target="_blank">download and import your official twitter archive</a> to see your oldest tweets.</li>
-	<li>Remember to <a href="https://github.com/amwhalen/archive-my-tweets#setting-up-a-cron-job" target="_blank">setup a cron job</a> so your newest tweets will continue to be fed into this archive.</li>
-	<li>If you need to make any changes to your settings, just modify the config.php file.</li>
+    <li>If you have more than 3200 tweets you'll need to <a href="https://github.com/amwhalen/archive-my-tweets#importing-your-official-twitter-archive" target="_blank">download and import your official twitter archive</a> to see your oldest tweets.</li>
+    <li>Remember to <a href="https://github.com/amwhalen/archive-my-tweets#setting-up-a-cron-job" target="_blank">setup a cron job</a> so your newest tweets will continue to be fed into this archive.</li>
+    <li>If you need to make any changes to your settings, just modify the config.php file.</li>
 </ul>
 
 <p><a href="" class="btn btn-primary btn-large">See My Twitter Archive &raquo;</a></p>
